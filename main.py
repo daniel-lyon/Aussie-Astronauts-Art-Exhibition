5caa892c34580b9d2d8d0dad4edc59e009db42f4
Daniel Lyon <daniellyon31@gmail.com>
2022-10-01 18:19:27 +1000

    Bunch o' changes

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import numpy as np
 import matplotlib.pylab as plt
 from music21 import note, stream
 import time
+from skimage.transform import resize
+from random import random
 
 # Read in the image
-image = plt.imread('test_image.jpg')/255
+image = plt.imread('test_image.jpg')
+# image = plt.imread('composite.png')
+# image = plt.imread('170x170.jpg')
+image = image/255
+image = resize(image, (200, 200))
 height = image.shape[0]
 width = image.shape[1]
 
@@ -14,13 +20,14 @@ note_names = ['C2','D2','E2','G2','A2',
              'C3','D3','E3','G3','A3',
              'C4','D4','E4','G4','A4',
              'C5','D5','E5','G5','A5']
-note_values = [round(i * 1/len(note_names)+0.05 ,3) for i in range(len(note_names))]
+note_values = [round(i * 1/len(note_names)+0.05, 3) for i in range(len(note_names))]
 
 # Volume settings
-volume_values = [i+1 for i in range(128)]
+volume_values = [i+1 for i in range(16, 80)]
 volume_norm = [i/128 for i in volume_values]
 
 start_time = time.process_time()
+print('start')
 
 # Setting the duration of each note
 duration_page = np.empty((height, width), dtype = int)
@@ -40,6 +47,7 @@ for y in range(width):
 
         elif value >= 0.75:
             duration_page[x][y] = 4
+print('duration done')
 
 # Setting the notes to be played
 notes_page = np.empty((height, width), dtype = list)
@@ -52,6 +60,7 @@ for y in range(width):
             if value <= note_values[index]:
                 notes_page[x][y] = notes
                 break
+print('notes done')
 
 # Setting the volume of each note
 volume_page = np.empty((height, width), dtype = int)
@@ -64,19 +73,23 @@ for y in range(width):
             if value <= volume_norm[index]:
                 volume_page[x][y] = volume
                 break
+print('volume done')
 
 # Combine notes with duration and volume
 full_audio = stream.Stream()
 for y in range(width):
     s = stream.Part()
+    r = round(random(), 1)
     for x in range(height):
         n = note.Note(notes_page[x][y], quarterLength = duration_page[x][y])
-        n.volume.velocity(volume_page[x][y])
-        s.append(n)
+        n.volume.velocity = volume_page[x][y]
+        s.insert(y+r, n)
     full_audio.append(s)
+print('audio done')
 
 # Export file
-full_audio.write('midi', 'piano.mid')
+full_audio.write('mid', 'piano.mid')
 
+# Time to execute program
 end_time = time.process_time()
 print(f'Finished in {end_time - start_time} seconds')
